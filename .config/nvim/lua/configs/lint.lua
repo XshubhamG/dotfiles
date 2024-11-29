9dddd6375a0c4f27e41cd65b78432777595b8f69
Shubham Giri <shubhammgiri3@gmail.com>
2024-11-29 13:22:17 +0530

    added fonts into dotfiles

--- .config/nvim/lua/configs/lint.lua
+++ .config/nvim/lua/configs/lint.lua
@@ -2,7 +2,7 @@ local lint = require "lint"
 
 lint.linters_by_ft = {
   -- lua = { "luacheck" },
-  javascript = { "eslint_d" },
+  -- javascript = { "eslint_d" },
   typescript = { "eslint_d" },
   javascriptreact = { "eslint_d" },
   typescriptreact = { "eslint_d" },
