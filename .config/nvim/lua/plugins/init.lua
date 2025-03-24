7990c421550ddfe8855d9c01671096546d2f1dc7
Shubham Giri <shubhammgiri3@gmail.com>
2025-03-24 17:10:42 +0530

    enable codeium autocompletion in nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -220,4 +220,23 @@ return {
       return require "configs.markdown"
     end,
   },
+
+  {
+    "Exafunction/codeium.vim",
+    event = "VeryLazy",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-n>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-p>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
+  },
 }
